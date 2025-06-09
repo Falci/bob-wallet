77fde22744d3f89183358a57e4ab0c44439fe68a
Fernando Falci <falci@falci.me>
2025-06-09 16:29:59 +0200

    fix(explorers): update Niami URLs and add new explorers

--- app/constants/explorers.js
+++ app/constants/explorers.js
@@ -1,14 +1,26 @@
 export const EXPLORERS = [
   {
+    label: 'hns.dev',
+    tx: 'https://explorer.hns.dev/tx/%s',
+    name: 'https://explorer.hns.dev/name/%s',
+    address: 'https://explorer.hns.dev/address/%s',
+  },
+  {
+    label: 'ShakeShift',
+    tx: 'https://shakeshift.com/transaction/%s',
+    name: 'https://shakeshift.com/name/%s',
+    address: 'https://shakeshift.com/address/%s',
+  },
+  {
     label: 'Niami',
     tx: 'https://www.niami.io/tx/%s',
-    name: 'https://www.niami.io/domain/%s',
+    name: 'https://www.niami.io/name/%s',
     address: 'https://www.niami.io/address/%s',
   },
   {
     label: 'Niami (on Handshake)',
     tx: 'https://niami/tx/%s',
-    name: 'https://niami/domain/%s',
+    name: 'https://niami/name/%s',
     address: 'https://niami/address/%s',
   },
   {
