49a15951797db5efa366dec418cca5f047558ad1
Fernando Falci <falci@falci.me>
2025-08-25 10:04:01 +0200

    chore(explorers): remove Niami entries from explorers list

--- app/constants/explorers.js
+++ app/constants/explorers.js
@@ -12,18 +12,6 @@ export const EXPLORERS = [
     address: 'https://shakeshift.com/address/%s',
   },
   {
-    label: 'Niami',
-    tx: 'https://www.niami.io/tx/%s',
-    name: 'https://www.niami.io/name/%s',
-    address: 'https://www.niami.io/address/%s',
-  },
-  {
-    label: 'Niami (on Handshake)',
-    tx: 'https://niami/tx/%s',
-    name: 'https://niami/name/%s',
-    address: 'https://niami/address/%s',
-  },
-  {
     label: 'HNS Fans',
     tx: 'https://e.hnsfans.com/tx/%s',
     name: 'https://e.hnsfans.com/name/%s',
